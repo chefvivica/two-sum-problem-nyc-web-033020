03cf4d490adeb7d920fd5d5b463276eafa52a714
Vivica Cong Ma <vivicacongma@gmail.com>
2020-10-13 01:24:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,21 @@
 //   else left++
 // }
 // }
-
-const bruteForceTwoSum = (array) => {
-  
+describe('#bruteForceTwoSum', function() {
+	it("collects each pair of numbers with a matching sum", function() {
+		let array = [2, 3, 4, 3, 6, 7]
+		let sum = 6
+		expect(bruteForceTwoSum(array, sum)).toEqual([[2, 4], [3, 3]])
+	})
+})
+const bruteForceTwoSum = (array, sum) => {
+  let list = []
+  for(let i = 0 ; i < array.length; i++){
+    for(let j = 1; j < array.length; j++){
+      if(array[i] + array[j] == sum){
+        list.push([i,j])
+      }
+    }
+  }
+  return list
 }

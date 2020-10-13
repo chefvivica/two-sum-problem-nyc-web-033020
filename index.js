6eab8c5c13ed3f2ed9725be4a23f97372b9549e4
Vivica Cong Ma <vivicacongma@gmail.com>
2020-10-13 01:26:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-// const twoSum = ( sum, a)=>{
-// let sorted = [...a].sort()
-// let left = 0
-// let right = sorted.length-1
-// while(left < right){
-//   let res = sorted[left] + sorted[right]
-//   if(res === sum ) return [sorted[left], sorted[right]]
-//   else if(res > sum) right--
-//   else left++
-// }
-// }
-
 const bruteForceTwoSum = (array, sum) => {
   let list = []
   for(let i = 0 ; i < array.length; i++){

119b75cb3e4db5edc0c5fbd9f35d2daa7ad969da
Vivica Cong Ma <vivicacongma@gmail.com>
2020-10-13 01:30:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const bruteForceTwoSum = (array, sum) => {
   let list = []
-  array= [...new set(array)]
+  array= [...new Set(array)]
   for(let i = 0 ; i < array.length; i++){
     for(let j = 1; j < array.length; j++){
       if(array[i] + array[j] == sum){
